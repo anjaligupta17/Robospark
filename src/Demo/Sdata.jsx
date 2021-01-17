2574920c349c452049d5dc9db658559978cc5939
Anjali Gupta <48510489+anjaligupta17@users.noreply.github.com>
2021-01-17 13:51:11 +0530

    Add files via upload

---
+++ src/Demo/Sdata.jsx
@@ -0,0 +1,37 @@
+import web from "../src/images/s1.jpg";
+import app from "../src/images/app.jpeg";
+import panel from "../src/images/panel.jpg";
+import digital from "../src/images/digital.jpg";
+import manufacturers from "../src/images/manufacturers.jpg";
+import software from "../src/images/software.jpg";
+const Sdata=[
+    {
+        imgsrc:web,
+        title:"Artificial Intelligent with solar ligth",
+    },
+    {
+       
+        imgsrc:app,
+        title:"Solar App" ,
+    },
+    {
+        imgsrc:digital,
+        title:"Digitalization",
+
+    },
+    {
+       
+        imgsrc:panel,
+        title:"Solar panel", 
+    },
+    {
+       
+        imgsrc:manufacturers,
+        title:"Look to gain from propriety technologies in PV Module in solar Energy", 
+    },
+    {
+     imgsrc:software,
+     title:"Arduino solar tracker",
+    },
+];
+export default Sdata;
